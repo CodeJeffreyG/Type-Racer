f2d0aa6c7ae47c2765d4f902bcc5069546a2fd9d
Jeffrey <jeffrey.graham600@gmail.com>
2022-07-17 11:53:22 -0400

    set State for text area & function to handle state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
 function App() {
+  const [text, setText] = useState({
+    words: "",
+    on: false,
+  });
+
+  function onChange(event: any) {
+    const { value, name } = event.target;
+    setText((prevText) => ({
+      ...prevText,
+      [name]: value,
+    }));
+  }
+
+  function onClick(text: string) {}
+
   return (
     <div>
       <h1>How fast do you type?</h1>
-      <textarea />
+      <textarea name="text" onChange={onChange} value={text.words} />
       <h4>Time remaining: ???</h4>
       <button>Start</button>
       <h1>Word count: ???</h1>
