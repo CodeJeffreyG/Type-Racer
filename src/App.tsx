8a7ae738c48b3036eb59f4b208dce642a2cd51b9
Jeffrey <jeffrey.graham600@gmail.com>
2022-07-18 04:02:58 -0400

    Set state for game

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React, { useState } from "react";
+import { time } from "console";
+import React, { useState, useEffect } from "react";
+import { isShorthandPropertyAssignment } from "typescript";
 
 function App() {
+  //state of string in text area
   const [text, setText] = useState("");
 
+  //set state for decrementing counter
+  const [timeRemaining, setTimeRemaining] = useState(5);
+
+  //state for if game is running
+  const [isGameRunning, setisGamerunning] = useState(false);
+
+  //decrement timer
+  useEffect(() => {
+    //set condition for set timeout to stop decrementing
+    if (isGameRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
+
+  //set state to textarea
   function onChange(event: any) {
     const { value } = event.target;
     setText(value);
   }
 
+  //logic to count words... ensures an empty string is not counted to length
   function calculateWordCount(text: any) {
     const wordsArr = text.trim().split(" ");
     return wordsArr.filter((word: any) => word !== "").length;
@@ -19,8 +40,8 @@ function App() {
     <div>
       <h1>How fast do you type?</h1>
       <textarea name="words" onChange={onChange} value={text} />
-      <h4>Time remaining: ???</h4>
-      <button onClick={() => calculateWordCount(text)}>Start</button>
+      <h4>{`Time remaining: ${timeRemaining}`}</h4>
+      <button onClick={() => setisGamerunning(true)}>Start</button>
       <h1>{`Word count: ???`}</h1>
     </div>
   );
